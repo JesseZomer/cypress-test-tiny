860d405b4c9e0ff5d9ed079206c4482c5a6ae1c0
Jesse Zomer <jessezomer@hotmail.com>
2021-08-13 14:51:26 +0200

    adds failing test

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -2,5 +2,7 @@
 describe('page', () => {
   it('works', () => {
     cy.visit('https://example.cypress.io')
+    cy.get('.banner').console('info').should('contain.text', 'Kitchen Sink');
+    cy.get('.banner').console(cy.get('.navbar-header')).should('contain.text', 'Kitchen Sink');
   })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,30 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+Cypress.Commands.add(
+    'console',
+    {
+        prevSubject: true,
+    },
+    (subject, method) => {
+        // the previous subject is automatically received
+        // and the commands arguments are shifted
+
+        // allow us to change the console method used
+        method = method || 'log'
+
+        if (typeof method === 'string') {
+            // log the subject to the console
+            console[method]('The subject is', subject);
+        } else {
+            console.log('subject is not our banner, but the first argument we gave to console:', subject);
+        }
+
+        // whatever we return becomes the new subject
+        //
+        // we don't want to change the subject so
+        // we return whatever was passed in
+        return subject
+    }
+)
